ddc170b5eaaaf15829780d364371106f5089e442
rozenstraws <zdceleste@gmail.com>
2018-10-22 22:09:21 -0300

    Ultimando detalles (en proceso).

--- scripts.js
+++ scripts.js
@@ -7,11 +7,19 @@ let coverPosition = cover.getBoundingClientRect().bottom;
 // Header Fixed
 window.onscroll = () => {
 
-    if (window.pageYOffset >= coverPosition){
+    if (window.pageYOffset >= coverPosition) {
         header.classList.add("header-fixed");
-        cover.style.height ="100vh";
+        cover.style.height = "100vh";
     }
-
 }
 
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
