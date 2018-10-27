64836f7a911e29043ddb60a3a455b055934311ad
rozenstraws <zdceleste@gmail.com>
2018-10-27 18:55:37 -0300

    Arreglos en Header y Members.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
 let header = document.getElementById("amde-header");
 let cover = document.getElementById("amde-cover");
 
-// Posición
+// Position
 let coverPosition = cover.getBoundingClientRect().bottom;
 
 // Header Fixed
 window.onscroll = () => {
-
     if (window.pageYOffset >= coverPosition) {
         header.classList.add("header-fixed");
         cover.style.height = "100vh";
